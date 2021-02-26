06d26217dc46435265fbd67c891d62b9d3b257d4
EC2 Default User <abcdquont1234@gmail.com>
2021-02-26 07:32:25 +0000

    blog index追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,27 @@
 class BlogsController < ApplicationController
 
+  def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+
   def new
     @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :body, :image)
   end
 
 end
