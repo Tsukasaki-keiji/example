283e56d0f13ce996119c1ee927e5813688f8287e
EC2 Default User <abcdquont1234@gmail.com>
2021-02-27 07:22:03 +0000

    blog show画像表示成功

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,7 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     @blog.save
+    redirect_to blogs_path
   end
 
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '80fea893a99d36a8e1ccc8b21ece7185f8774a8969a781dd499aa0cb3a2aa6e8f691570358ac7a40517bd2164dd92a0d442ae8de111bf5bfc48d134633ecf9a8'
