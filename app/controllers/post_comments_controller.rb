fdf5a80e2fd510430f9b9c87eb30ae80ed9422f5
EC2 Default User <abcdquont1234@gmail.com>
2021-03-01 09:45:19 +0000

    コメント機能追記

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,9 +1,23 @@
 class PostCommentsController < ApplicationController
 
   def create
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    if @post_comment.save!
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    redirect_to blog_path(params[:blog_id])
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:post_comment_content, :blog_id)
   end
 
 end
