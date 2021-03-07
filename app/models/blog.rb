f135126de994fb2c1aae4996911515a68c7ed965
EC2 Default User <abcdquont1234@gmail.com>
2021-03-07 09:56:15 +0000

    favorite model作成

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,5 +6,9 @@ class Blog < ApplicationRecord
   attachment :image
   validates :image, presence: true
   validates :title, :body, presence: true
+  has_many :favorites, dependent: :destroy
 
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_091807) do
+ActiveRecord::Schema.define(version: 2021_03_07_095250) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_03_03_091807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "post_comments"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
